930b71bf2f2d2212459a7570627d78cf883882ba
SergioEstebanPi <sepi_147@hotmail.com>
2019-05-26 13:09:01 -0500

    Agrego tabla de datos para formulario basico

---
+++ src/components/Noticias.vue
@@ -0,0 +1,117 @@
+<template>
+  <v-container>
+    <h1 class="display-1 text-xs-center">Publicaciones recientes</h1>
+    <v-layout wrap>
+      <v-flex v-for="publicacion in publicaciones"
+              :key="publicacion.title"
+              v-bind="{ [`xs${publicacion.flex}`]: true }" mt-5>
+        <v-card>
+          <v-img
+            :src="publicacion.src"
+            height="200px"
+          >
+            <v-container
+              fill-height
+              fluid
+              pa-2
+            >
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text" v-text="publicacion.title"></span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+              <v-icon>favorite</v-icon>
+            </v-btn>
+            <v-btn icon>
+              <v-icon>bookmark</v-icon>
+            </v-btn>
+            <v-btn icon>
+              <v-icon>share</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout class="mt-5 secondary" justify-center align-center style="height:300px">
+      <v-flex class="info" xs3>
+        <h1>centrado</h1>
+      </v-flex>
+      <v-flex class="success" xs3>
+        <h1>centrado</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    publicaciones: [
+      { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
+      { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+      { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 }
+    ],
+    ecosystem: [
+      {
+        text: 'vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader'
+      },
+      {
+        text: 'github',
+        href: 'https://github.com/vuetifyjs/vuetify'
+      },
+      {
+        text: 'awesome-vuetify',
+        href: 'https://github.com/vuetifyjs/awesome-vuetify'
+      }
+    ],
+    importantLinks: [
+      {
+        text: 'Documentation',
+        href: 'https://vuetifyjs.com'
+      },
+      {
+        text: 'Chat',
+        href: 'https://community.vuetifyjs.com'
+      },
+      {
+        text: 'Made with Vuetify',
+        href: 'https://madewithvuetifyjs.com'
+      },
+      {
+        text: 'Twitter',
+        href: 'https://twitter.com/vuetifyjs'
+      },
+      {
+        text: 'Articles',
+        href: 'https://medium.com/vuetify'
+      }
+    ],
+    whatsNext: [
+      {
+        text: 'Explore components',
+        href: 'https://vuetifyjs.com/components/api-explorer'
+      },
+      {
+        text: 'Select a layout',
+        href: 'https://vuetifyjs.com/layout/pre-defined'
+      },
+      {
+        text: 'Frequently Asked Questions',
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
+      }
+
+    ]
+  })
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import List from '@/components/List'
+import Noticias from '@/components/Noticias'
 import Publicaciones from '@/components/Publicaciones'
 
 Vue.use(Router)
@@ -9,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'List',
-      component: List
+      name: 'Noticias',
+      component: Noticias
     },
     {
       path: '/publicaciones',
